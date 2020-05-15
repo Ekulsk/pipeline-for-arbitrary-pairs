0a5d8a6b9d68cfc3c45bb9ac861ca2825388e5fe
Lucca <ekulsk@gmail.com>
2020-05-15 05:14:09 -0400

    Updating main for csv

--- src/extractor/main/Main.java
+++ src/extractor/main/Main.java
@@ -10,16 +10,25 @@ public class Main {
 		System.out.println("Argument List:");
 		System.out.println("1. Root folder");
 		System.out.println("2. Out folder");
+		System.out.println("3. (optional) Output csv");
 		
 		String root = args[0];
 		String out = args[1];
+		String csv = "";
+		
 		
 		System.out.println("Root Folder: "+root);
 		System.out.println("Out Folder: "+out);
 
+		if (args.length > 2)
+		{
+			csv=args[2];
+			System.out.println("Out csv: "+csv);
+		}
+
 		CommitAnalyzer analyzer = new CommitAnalyzer();
 		
-		analyzer.analyzeCommits(root, out);
+		analyzer.analyzeCommits(root, out, csv);
 	}
 
 }
